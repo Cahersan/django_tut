93c0e6d1751a28760b766cd38dc860e38016276e
Carlos <cahersan@gmail.com>
2016-04-11 12:41:12 +0200

    Creado formulario de comentarios

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
-from .models import Post
-from .forms import PostForm
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
 # Create your views here.
 
 def post_list(request):
@@ -11,7 +11,24 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post}) 
+    comments = Comment.objects.filter(post=post).order_by('created_date')
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+
+    return render(request, 'blog/post_detail.html', {'post': post,
+                                                     'comments': comments,
+                                                     'form': form }) 
 
 def post_new(request):
     if request.method == "POST":
