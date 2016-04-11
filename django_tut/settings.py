52f3e213e43a3e799d289f96a8f9abc5bb7ac07f
Carlos <cahersan@gmail.com>
2016-04-11 10:32:50 +0200

    Usando MySQL

--- django_tut/settings.py
+++ django_tut/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'django_tut.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'HOST': 'localhost',
+        'NAME': 'django_tut',
+        'USER': 'carlos',
+        'PASSWORD': 'carlos',
+        'ATOMIC_REQUESTS': True,
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
