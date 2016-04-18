cbe8b579178ef68ff4efdec0baaa90c5b6e73363
Carlos <cahersan@gmail.com>
2016-04-18 15:02:34 +0200

    Limpieza

--- blog/views.py
+++ blog/views.py
@@ -7,12 +7,8 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 
 from .models import Post, Comment
 from .forms import CommentForm
-# Create your views here.
 
 
-#def post_list(request):
-#   posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-#   return render(request, 'blog/post_list.html', {"posts": posts})
 class LoginRequiredMixin(object):
     @classmethod
     def as_view(cls, **initkwargs):
@@ -66,57 +62,6 @@ class PostCreate(LoginRequiredMixin, CreateView):
         return reverse('post_detail', kwargs={'pk': self.object.pk })
 
 
-# def post_detail(request, pk):
-#     post = get_object_or_404(Post, pk=pk)
-#     comments = Comment.objects.filter(post=post).order_by('created_date')
-
-#     if request.method == "POST":
-#         if request.user.is_authenticated:
-#             form = CommentForm(request.POST)
-#             if form.is_valid():
-#                 comment = form.save(commit=False)
-#                 comment.author = request.user
-#                 comment.post = post
-#                 comment.save()
-
-#                 return redirect('post_detail', pk=post.pk)
-#         else:
-#             return redirect('login')
-#     else:
-#         form = CommentForm()
-
-#    return render(request, 'blog/post_detail.html', {'post': post,
-#                                                      'comments': comments,
-#                                                      'form': form }) 
-
-# @login_required(login_url='login')
-# def post_new(request):
-#     if request.method == "POST":
-#         form = PostForm(request.POST)
-
-#         if form.is_valid():
-#             post = form.save(commit=False)
-#             post.author = request.user
-#             post.publish()
-#             return redirect('post_detail', pk=post.pk)
-#     else:
-#         form = PostForm()
-#     return render(request, 'blog/post_edit.html', {"form": form})
-
-# @login_required(login_url='login')
-# def post_edit(request, pk):
-#     post = get_object_or_404(Post, pk=pk)
-#     if request.method == "POST":
-#         form = PostForm(request.POST, instance=post)
-#         if form.is_valid():
-#             post = form.save(commit=False)
-#             post.author = request.user
-#             post.save()
-#             return redirect('post_detail', pk=post.pk)
-#     else:
-#         form = PostForm(instance=post)
-#     return render(request, 'blog/post_edit.html', {'form': form, 'post':post})
-
 class PostEdit(LoginRequiredMixin, UpdateView):
     model = Post
     fields = ['title', 'text']
@@ -125,12 +70,6 @@ class PostEdit(LoginRequiredMixin, UpdateView):
     def get_success_url(self):
         return reverse('post_detail', kwargs={'pk': self.object.pk })
 
-
-# @login_required(login_url='login')
-# def post_delete(request, pk):
-#     post = get_object_or_404(Post, pk=pk)
-#     post.delete()
-#     return redirect('post_list')
 
 class PostDelete(LoginRequiredMixin, DeleteView):
     model = Post
--- django_tut/urls.py
+++ django_tut/urls.py
@@ -3,12 +3,9 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'django_tut.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^sobre-mi/', TemplateView.as_view(template_name="sobre-mi.html"), name="about"),
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('blog.urls')),
+    url(r'^sobre-mi/', TemplateView.as_view(template_name="sobre-mi.html"), name="about"),
 	url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page':'/'}),
     url('^', include('django.contrib.auth.urls')),
 ]
